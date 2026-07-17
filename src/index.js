e8244fd0ae35694fde9e621c5a32fd7d8771e195
Sangwanji <sakshamsangwan2005@outlook.com>
2026-07-17 20:29:54 +0530

    Basic Structure SetUp

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express =require('express');
+const {PORT} = require('./config/serverConfig');
+const apiRoutes= require('./routes/index');
+const db =require('./models/index');
+const prepareAndStartServer=async ()=>{
+    const app=express();
+    app.use(express.json());
+    app.use(express.urlencoded({extended:true}));
+    app.use('/api',apiRoutes);
+    app.listen(PORT,()=>{
+        console.log(`Server Start on Port No ${PORT}`);
+        if(process.env.DB_SYNC) db.sequelize.sync({alter:true});
+    });
+
+
+}
+
+prepareAndStartServer();
